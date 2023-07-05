7129918d60a62561d895d0e451bf0a1c9c0f5398
1M-GA <athrv.gajbhiye.xxii@gmail.com>
2023-07-05 14:30:50 +0530

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const path = require('path');
+
+
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -6,8 +9,11 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const e = require('express');
 
 app.use(bodyParser.urlencoded( {extended: false}));
+
+app.use(express.static(path.join(__dirname,'public')))
 
 
 app.use('/admin',adminRoutes);
@@ -15,7 +21,7 @@ app.use(shopRoutes);
 
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'Views','404.html'))
 })
 
-app.listen(5000); 
+app.listen(3000); 
